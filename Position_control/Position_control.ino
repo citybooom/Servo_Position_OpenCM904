a175c4a4089317e2cc5bf039c813052c5029064d
citybooom <tchengwpi@outlook.com>
2018-12-14 19:41:43 -0500

    Update Position_control.ino

--- Position_control/Position_control.ino
+++ Position_control/Position_control.ino
@@ -1,28 +1,10 @@
-/*******************************************************************************
- * Copyright 2016 ROBOTIS CO., LTD.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
-
-/* Authors: Taehun Lim (Darby) */
-#define DEVICE_NAME "1" //Dynamixel on Serial3(USART3)  <-OpenCM 485EXP
+#define DEVICE_NAME "1" 
 #include <DynamixelWorkbench.h>
 
 #include <ros.h>
 #include <std_msgs/Int16MultiArray.h>
 
 #define BAUDRATE  1000000
-//#define DXL_ID    3
 
 DynamixelWorkbench dxl_wb;
 
